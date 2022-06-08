cb4b59222c25e5dbe70b8888ebc7c2bd5a2d741d
PrimeTime416 <a2kerr@gmail.com>
2022-06-07 22:12:05 -0400

    rotary encoder liberary

--- Thonny/Game_Invaders/main_ivaders.py
+++ Thonny/Game_Invaders/main_ivaders.py
@@ -8,6 +8,34 @@ from time import sleep_ms
 import framebuf
 import random
 
+# The MIT License (MIT)
+# Copyright (c) 2021 Mike Teachman
+# https://opensource.org/licenses/MIT
+
+# example for MicroPython rotary encoder
+
+import sys
+if sys.platform == 'esp8266' or sys.platform == 'esp32':
+    from rotary_irq_esp import RotaryIRQ
+elif sys.platform == 'pyboard':
+    from rotary_irq_pyb import RotaryIRQ
+elif sys.platform == 'rp2':
+    from rotary_irq_rp2 import RotaryIRQ
+else:
+    print('Warning:  The Rotary module has not been tested on this platform')
+
+import time
+
+
+r = RotaryIRQ(pin_num_clk=37,
+              pin_num_dt=36,
+              min_val=-64,
+              max_val=64,
+              reverse=False,
+              range_mode=RotaryIRQ.RANGE_WRAP)
+
+
+
 # Resilution 8192px 
 WIDTH  = 128                                            # oled display width
 HEIGHT = 64                                             # oled display height
@@ -19,12 +47,9 @@ i2c = SoftI2C(scl=Pin(15), sda=Pin(4))
 oled = SSD1306_I2C(WIDTH, HEIGHT, i2c)                  # Init oled display
 
 sWidth =1
-select1 = Signal(Pin(38, Pin.IN), invert=True) 
-# select1 = Pin(38, Pin.IN) 
-roteA = Pin(37, Pin.IN)
-roteB = Pin(36, Pin.IN)
-
-
+select1 = Signal(Pin(38, Pin.IN), invert=True)
+val_old = 0
+r.set(value= -124)
 
 pattern = [
     '0b110000001100000011111111111111111100000011000000', 
@@ -88,7 +113,7 @@ def compileMatrix(matrixString, shift):
 x = {
     'ship' : pattern[5],
     'shift' : 0,
-    'home' : 5
+    'home' : 60
     }
 
 y = {
@@ -100,36 +125,40 @@ y = {
 
 shipsX = [x, y]
 
-# # Clear the oled display in case it has junk on it.
-# def renderX(matrixString):
-#     for runX in range(11):
-#         oled.fill(0)
-#         for x in matrixString:
-#             # print(y)
-#             # print(x)
-#             oled.blit(compileMatrix(x['ship'], x['shift']), x['home'], runX)
-#             # oled.blit(compileMatrix(x[1]['ship'], x[1]['shift']), 15, runX)
-#             # oled.blit(compileMatrix(matrixString), 5, x)
-#         # Finally update the oled display so the image & text is displayed
-#         sleep(.01)
-#         # print(x)
-#         oled.show()
+
+
+
+def controls(objects = []):
+    print('IN: controls: {}'.format(objects[0]))
+    global val_old
+    val_new = r.value()
+    if val_old != val_new:
+        deltaX = val_old - val_new
+        print('delta: {2} - {3} = {1} home: {0}'.format(deltaX, objects[0]['home'], val_old, val_new))
+        val_old = val_new
+        objects[0]['home'] += deltaX
+#         print('delta: {2} - {3} = {1} home: {0}'.format(deltaX, objects[0]['home'], val_old, val_new))
+        print('home: {0}'.format(objects[0]['home']))     
 
 # Clear the oled display in case it has junk on it.
 def renderX(matrixString):
-    runX = 0
-    for sysTic in range(500):
-        runX += select1.value()
-#         print('select1: {}'.format(runX))
-#         print('roteA: {}'.format(roteA.value()))
-#         print('roteB: {}'.format(roteB.value()))
+#     print('IN: renderX')
+    global val_old
+    for sysTic in range(5000):
+        controls(matrixString)
         oled.fill(0)
-        for x in matrixString:
-            oled.blit(compileMatrix(x['ship'], x['shift']), x['home'], runX)
+        oled.blit(compileMatrix(matrixString[0]['ship'], matrixString[0]['shift']), matrixString[0]['home'], 0)
         sleep_ms(16)
         oled.show()
 
 renderX(shipsX)
+# def test(c = 0):
+#     print('TEST START')
+#     for x in range(c):
+#         val_old = r.value()
+#         controls()
+#         sleep_ms(16)
+#     print('TEST END')    
+# 
+# test(5000)
 print("RUN COMPLETE")        
-
-
